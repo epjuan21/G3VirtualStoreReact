cd1dcf59ab8cc47c74f4c68864b069da0c0c2934
Juan Fernando Ramirez <ep.juan@gmail.com>
2021-11-23 14:01:54 -0500

    Correccion en user.router.js

--- server/src/routes/users.router.js
+++ server/src/routes/users.router.js
@@ -6,6 +6,6 @@ const verifySignUp = require('../middlewares/verifySignUp');
 
 router.get('/', userController.getUsers);                                                                                // Get Users
 router.post('/', [authJwt.verifyToken, authJwt.isAdmin, verifySignUp.checkRolesExisted], userController.addUser)         // Add User
-router.put('/:id', [authJwt.verifyToken, authJwt.isAdmin, verifySignUp.checkRolesExisted], userController.updateUser)    // Update User
+router.put('/:id', [authJwt.verifyToken, verifySignUp.checkRolesExisted], userController.updateUser)    // Update User
 
 module.exports = router;
