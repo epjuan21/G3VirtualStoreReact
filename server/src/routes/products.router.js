2a12ae6cb902b0b98329bd84c6b2c030f1f1f2b0
Juan Fernando Ramirez <ep.juan@gmail.com>
2021-11-23 14:02:08 -0500

    Correccion en products.router.js

--- server/src/routes/products.router.js
+++ server/src/routes/products.router.js
@@ -6,8 +6,8 @@ const authJwt = require('../middlewares/authJwt');
 router.get('/', productsController.getProducts)                                                 // Return All Products
 router.get('/:id', productsController.getProductById)                                           // Get Product By Id
 router.get('/category/:categoryId', productsController.getProductByCategory)                    // Get Products By Category Id
-router.post('/', [authJwt.verifyToken, authJwt.isAdmin], productsController.addProduct)         // Add Product
-router.put('/:id', [authJwt.verifyToken, authJwt.isAdmin], productsController.updateProduct)    // Update Product
+router.post('/', [authJwt.verifyToken], productsController.addProduct)         // Add Product
+router.put('/:id', [authJwt.verifyToken], productsController.updateProduct)    // Update Product
 router.delete('/:id', [authJwt.verifyToken, authJwt.isAdmin], productsController.deleteProduct) // Delete Product
 
 module.exports = router;
