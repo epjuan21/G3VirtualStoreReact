8d5032635e58d226f132d34a07671cc73fbc50e3
Juan Fernando Ramirez <ep.juan@gmail.com>
2021-11-24 10:57:44 -0500

    Validaciones en categories.model

--- server/src/models/categories.model.js
+++ server/src/models/categories.model.js
@@ -4,27 +4,27 @@ const { Schema } = mongoose;
 const CategoriesSchema = new Schema({
         title: {
             type: String,
-            required: true,
+            required: [true, "Category title is required"],
             max: 60
         },
         name: {
             type: String,
-            required: true,
+            required: [true, "Category name is required"],
             max: 60
         },
         slug: {
             type: String,
-            required: true,
+            required: [true, "The category slug is required"],
             max: 60
         },
         description: {
             type: String,
-            required: true,
+            required: [true, "The description of the category is required"],
             max: 255
         },
         imageUrl: {
             type: String,
-            required: true,
+            required: [true, "The category image is required"],
             max: 255
         }
     },
