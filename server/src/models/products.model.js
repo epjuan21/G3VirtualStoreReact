24d93ee83194b91bf65e46ba1e304548995c5172
Juan Fernando Ramirez <ep.juan@gmail.com>
2021-11-24 10:46:52 -0500

    Validaciones en products.model

--- server/src/models/products.model.js
+++ server/src/models/products.model.js
@@ -4,21 +4,21 @@ const { Schema } = mongoose;
 const ProductsSchema = new Schema({
         name: {
             type: String,
-            required: true,
+            required: [true, "Product name is required"],
             max: 100
         },
         price: {
             type: Number,
-            required: true,
+            required: [true, "The price of the product is required"],
         },
         description: {
             type: String,
-            required: true,
+            required: [true, "Product description is required"],   
             max: 255
         },
         imageUrl: {
             type: String,
-            required: true,
+            required: [true, "Product image is required"],
             max: 255
         }
         ,category_id: {
