c7481f33571d805207c9d5b9fd181149056bc005
Juan Fernando Ramirez <ep.juan@gmail.com>
2021-11-26 13:39:49 -0500

    Mejora de validaciones en el modelo categories.model

--- server/src/models/categories.model.js
+++ server/src/models/categories.model.js
@@ -6,29 +6,33 @@ const CategoriesSchema = new Schema({
             type: String,
             required: [true, "Category title is required"],
             unique: [true, "The category title must be unique"],
-            max: 60
+            minlength: [10, "The title must be more than 10 characters"],
+            maxlength: [100,"The title must be less than 500 characters"]
         },
         name: {
             type: String,
             required: [true, "Category name is required"],
             unique: [true, "The category name must be unique"],
-            max: 60
+            minlength: [10, "The name must be more than 10 characters"],
+            maxlength: [100,"The name must be less than 100 characters"]
         },
         slug: {
             type: String,
             required: [true, "The category slug is required"],
             unique: [true, "The category slug must be unique"],
-            max: 60
+            minlength: [20, "The slug must be more than 10 characters"],
+            maxlength: [100,"The slug must be less than 500 characters"]
         },
         description: {
             type: String,
             required: [true, "The description of the category is required"],
-            max: 255
+            minlength: [20, "The description must be more than 20 characters"],
+            maxlength: [500,"The description must be less than 500 characters"]
         },
         imageUrl: {
             type: String,
             required: [true, "The category image is required"],
-            max: 255
+            maxlength: [500,"The imageUrl must be less than 500 characters"]
         }
     },
     {
