a13ca1e673e2445d44e5e09887f075a418faf3c6
Juan Fernando Ramirez <ep.juan@gmail.com>
2021-11-26 14:26:22 -0500

    Actualizacion metodo HTTP para actualizar perfil de usuario en users.router.js

--- server/src/routes/users.router.js
+++ server/src/routes/users.router.js
@@ -4,8 +4,9 @@ const userController = require('../controllers/users.controller');
 const authJwt = require('../middlewares/authJwt');
 const verifySignUp = require('../middlewares/verifySignUp');
 
-router.get('/', userController.getUsers);                                                                                // Get Users
-router.post('/', [authJwt.verifyToken, authJwt.isAdmin, verifySignUp.checkRolesExisted], userController.addUser)         // Add User
-router.put('/:id', [authJwt.verifyToken, verifySignUp.checkRolesExisted], userController.updateUser)    // Update User
+router.get('/', userController.getUsers);                                                                               // Get Users
+router.get('/:id', userController.getUserById);                                                                         // Get User By Id
+router.post('/', [authJwt.verifyToken, authJwt.isAdmin, verifySignUp.checkRolesExisted], userController.addUser)        // Add User
+router.post('/:id', authJwt.verifyToken, userController.updateUser)                                                     // Update User
 
 module.exports = router;
